0aa8d474ac87f0cbc3408c6c57873ed8597c3f40
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2023-01-17 11:10:51 -0300

    yah

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { React, useEffect } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { BlurView } from "expo-blur";
+
+const uri =
+  "https://s3.amazonaws.com/exp-icon-assets/ExpoEmptyManifest_192.png";
 
 export default function App() {
+  const text = "Hello world!";
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={[StyleSheet.absoluteFill, styles.image]} source={{ uri }} />
+
+      <BlurView intensity={80} tint="light" style={styles.blurContainer}>
+        <Text style={styles.text}>{text}</Text>
+      </BlurView>
+      <BlurView intensity={90} style={styles.blurContainer}>
+        <Text style={styles.text}>{text}</Text>
+      </BlurView>
+      <BlurView intensity={100} tint="dark" style={styles.blurContainer}>
+        <Text style={[styles.text, { color: "#fff" }]}>{text}</Text>
+      </BlurView>
     </View>
   );
 }
@@ -14,8 +28,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+
+  text: {
+    fontSize: 68,
+  },
+  blurContainer: {},
 });
