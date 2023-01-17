797a4040fd39cb34a81db29b848a590a7e369bff
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2023-01-17 11:19:40 -0300

    Update App.js

--- App.js
+++ App.js
@@ -12,10 +12,10 @@ export default function App() {
     <View style={styles.container}>
       <Image style={[StyleSheet.absoluteFill, styles.image]} source={{ uri }} />
 
-      <BlurView intensity={80} tint="light" style={styles.blurContainer}>
+      <BlurView intensity={50} tint="light" style={styles.blurContainer}>
         <Text style={styles.text}>{text}</Text>
       </BlurView>
-      <BlurView intensity={90} style={styles.blurContainer}>
+      <BlurView intensity={75} style={styles.blurContainer}>
         <Text style={styles.text}>{text}</Text>
       </BlurView>
       <BlurView intensity={100} tint="dark" style={styles.blurContainer}>
